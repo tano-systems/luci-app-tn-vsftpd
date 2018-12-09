db677b605a41465da4f19795db659645eaacdd90
Anton Kikin <a.kikin@tano-systems.com>
2018-12-10 01:54:09 +0300

    Fix logging options
    The vsftpd service cannot simultaneously log into both a file and a syslog.
    
    Checkboxes for file logging and syslog logging replaced by one combobox with
    "disabled", "to file" and "to syslog" options.
    
    Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

--- luasrc/model/cbi/vsftpd/log.lua
+++ luasrc/model/cbi/vsftpd/log.lua
@@ -16,14 +16,45 @@ m = Map("vsftpd", translate("FTP Server: Log Settings"))
 
 sl = m:section(NamedSection, "log", "log", translate("Log Settings"))
 
-o = sl:option(Flag, "syslog", translate("Enable syslog"))
-o.default = false
+o = sl:option(ListValue, "mode",
+	translate("Enable logging"))
 
-o = sl:option(Flag, "xreflog", translate("Enable file log"))
-o.default = true
+o:value("disabled", translate("disable"))
+o:value("file",     translate("to file"))
+o:value("syslog",   translate("to syslog"))
+o.default = "file"
+
+function o.cfgvalue(self, section)
+	local syslog = self.map.uci:get_bool("vsftpd", section, "syslog")
+	local file   = self.map.uci:get("vsftpd", section, "file")
+
+	if syslog and syslog == true then
+		return "syslog"
+	elseif file and file ~= "" then
+		return "file"
+	else
+		return "disabled"
+	end
+end
+
+function o.write(self, section, value)
+	ListValue.write(self, section, value)
+	if value == "file" then
+		self.map.uci:set("vsftpd", section, "syslog", false)
+		self.map.uci:set("vsftpd", section, "xferlog", true)
+	elseif value == "syslog" then
+		self.map.uci:set("vsftpd", section, "syslog", true)
+		self.map.uci:set("vsftpd", section, "xferlog", true)
+	else
+		self.map.uci:set("vsftpd", section, "syslog", false)
+		self.map.uci:set("vsftpd", section, "xferlog", false)
+		self.map.uci:set("vsftpd", section, "file", "")
+	end
+end
 
 o = sl:option(Value, "file", translate("Log file"))
 o.default = "/var/log/vsftpd.log"
-
+o.rmempty = true
+o:depends("mode", "file")
 
 return m
